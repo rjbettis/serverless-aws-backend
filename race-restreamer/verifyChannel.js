9ec00abba115003d56f523f605e7799ad6671d71
Ryan Bettis <rbettis2@asu.edu>
2020-06-25 02:36:33 -0700

    set verify-channel param and generated new key for env file

--- race-restreamer/verifyChannel.js
+++ race-restreamer/verifyChannel.js
@@ -4,11 +4,11 @@ const fetch = require('node-fetch');
 
 module.exports.endpoint = async (event, context, callback) => {
   const response = await fetch(
-    'https://api.twitch.tv/kraken/users?login=petedorr',
+    'https://api.twitch.tv/kraken/users?login=' + event.channel,
     {
       headers: {
         accept: 'application/vnd.twitchtv.v5+json',
-        'client-id': 'nfdjz9d3s5bgrbbld2dhdsqnx3x9qo',
+        'client-id': process.env.TWITCH_ID,
       },
     }
   );
@@ -16,5 +16,3 @@ module.exports.endpoint = async (event, context, callback) => {
 
   callback(null, res);
 };
-
-//https://dh470k8a55.execute-api.us-east-1.amazonaws.com/dev/verify-channel?channel=petedorr
